5b626f1a6af12a2f98e3d748972e0c66d95509b4
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2022-11-09 10:50:10 +0100

    migrations

--- pywww/main/models.py
+++ pywww/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    # text field with constrains in length
+    content = models.TextField()
+    # text field without constrains in length
+    published = models.BooleanField(default=False)
+    # true/false flag
+    created = models.DateTimeField(auto_now_add=True)
+    # data utworzenia - tylko przy utworzeniu
+    modified = models.DateTimeField(auto_now=True)
+
+
+
